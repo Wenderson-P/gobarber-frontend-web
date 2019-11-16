56fdc96fd7ab2fc15f841c774dcd6937604e6c97
Wenderson <wps713@gmail.com>
2019-11-16 18:17:20 -0300

    Validating Sign In and Up with Yup

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,21 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import * as Yup from "yup";
 import { Form, Input } from "@rocketseat/unform";
 
 import logo from "~/assets/logo.svg";
+
+const schema = Yup.object().shape({
+  name: Yup.string().required("O seu nome é obrigatório"),
+  email: Yup.string()
+    .email("Insira um e-mail válido")
+    .required("O e-mail é obrigatório"),
+  password: Yup.string()
+    .required("A senha é obrigatória")
+    .min(6, "No mínimo 6 caracteres")
+});
 
 export default function SignUp() {
   function handleSubmit(data) {
     console.tron.log(data);
   }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <Form onSubmit={handleSubmit}>
-        <Input name="text" type="text" placeholder="Nome completo" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
-        <button type="submit">Ciar conta</button>
+        <button type="submit">Criar conta</button>
         <Link to="/">Já sou cadastrado</Link>
       </Form>
     </>
