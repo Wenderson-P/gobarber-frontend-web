36aae4fdf353e50f0d639fa37a53fffab17a22d5
Wenderson <wps713@gmail.com>
2019-11-16 18:03:35 -0300

    Add signup content

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-// import { Container } from './styles';
+import logo from "~/assets/logo.svg";
 
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input type="text" placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+        <button type="submit">Ciar conta</button>
+        <Link to="/">Já sou cadastrado</Link>
+      </form>
+    </>
+  );
 }
