b7399414640651682f35fa088a8bdca01fcbce6d
Wenderson <wps713@gmail.com>
2019-12-14 19:44:10 -0300

    Add notification component

---
+++ src/components/Notifications/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { MdNotifications } from "react-icons/md";
+import { Container, Badge, NotificationList, Notification } from "./styles";
+
+export default function Notifications() {
+  return (
+    <Container>
+      <Badge hasUnread>
+        <MdNotifications color="#7159c1" size={20} />
+      </Badge>
+      <NotificationList>
+        <Notification unread>
+          <p>Você possui um novo agendamento para amanhã</p>
+          <time>há dois dias</time>
+          <button type="button">Marcar como lidar</button>
+        </Notification>
+        <Notification>
+          <p>Você possui um novo agendamento para amanhã</p>
+          <time>há dois dias</time>
+          <button type="button">Marcar como lidar</button>
+        </Notification>
+        <Notification>
+          <p>Você possui um novo agendamento para amanhã</p>
+          <time>há dois dias</time>
+          <button type="button">Marcar como lidar</button>
+        </Notification>
+      </NotificationList>
+    </Container>
+  );
+}
