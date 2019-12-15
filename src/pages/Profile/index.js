ffc4cb6cf5989e3221def8340e27c4ff440f8035
Wenderson <wps713@gmail.com>
2019-12-15 18:27:57 -0300

    Add profile page content

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,30 @@
 import React from "react";
-
-// import { Container } from './styles';
+import { Form, Input } from "@rocketseat/unform";
+import { Container } from "./styles";
 
 export default function Profile() {
-  return <div />;
+  return (
+    <Container>
+      <Form>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de email" />
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Confirm your password"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="submit">Sair do GoBarber</button>
+    </Container>
+  );
 }
