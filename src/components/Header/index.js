797e764320c62cb2a23b11adf75cbd5f5c5431d6
Wenderson <wps713@gmail.com>
2020-01-18 12:25:48 -0300

    Fix alt header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -27,7 +27,7 @@ export default function Header() {
               src={
                 profile.avatar.url || "https://api.adorable.io/avatars/50/.png"
               }
-              alt="Wenderson Pacheco"
+              alt={profile.name}
             />
           </Profile>
         </aside>
